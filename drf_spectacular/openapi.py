f444d3d1a0f3e6a34f576ceb95ba179f22e7c9c1
John Vandenberg <jayvdb@gmail.com>
2020-04-25 10:49:25 +0700

    Add drf-gis fields

--- drf_spectacular/openapi.py
+++ drf_spectacular/openapi.py
@@ -20,6 +20,7 @@ from rest_framework.views import APIView
 from drf_spectacular.fields import OpenApiSerializerFieldExtension
 from drf_spectacular.settings import spectacular_settings
 from drf_spectacular.contrib.authentication import *  # noqa: F403, F401
+from drf_spectacular.contrib.fields import *  # noqa: F403, F401
 from drf_spectacular.contrib.serializers import *  # noqa: F403, F401
 from drf_spectacular.plumbing import (
     build_basic_type, warn, anyisinstance, force_instance, is_serializer,
--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -1,4 +1,5 @@
 from rest_framework import fields, serializers, viewsets, mixins
+from rest_framework_gis.fields import GeometryField, GeometrySerializerMethodField
 
 from drf_spectacular.fields import OpenApiSerializerFieldExtension
 from drf_spectacular.plumbing import build_basic_type
@@ -11,7 +12,7 @@ class Base64Field(fields.Field):
     pass  # pragma: no cover
 
 
-def test_serializer_field_extension(no_warnings):
+def test_serializer_field_extension_base64(no_warnings):
     class Base64FieldExtension(OpenApiSerializerFieldExtension):
         target_class = 'tests.test_extensions.Base64Field'
 
@@ -26,5 +27,39 @@ def test_serializer_field_extension(no_warnings):
 
     schema = generate_schema('x', XViewset)
     validate_schema(schema)
-    schema['components']['schemas']['X']['properties']['hash']['type'] == 'string'
-    schema['components']['schemas']['X']['properties']['hash']['format'] == 'byte'
+    assert schema['components']['schemas']['X']['properties']['hash']['type'] == 'string'
+    assert schema['components']['schemas']['X']['properties']['hash']['format'] == 'byte'
+
+
+def test_serializer_field_extension_geo(no_warnings):
+    class XSerializer(serializers.Serializer):
+        location = GeometryField()
+
+    class XViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+        serializer_class = XSerializer
+
+    schema = generate_schema('x', XViewset)
+    validate_schema(schema)
+    assert isinstance(schema['components']['schemas']['X']['properties']['location'], dict)
+    assert len(schema['components']['schemas']['X']['properties']['location']['oneOf']) == 2
+    assert schema['components']['schemas']['X']['properties']['location']['oneOf'][0]['type'] == 'object'
+    assert schema['components']['schemas']['X']['properties']['location']['oneOf'][0]['required'] == ['type', 'coordinates']
+    assert schema['components']['schemas']['X']['properties']['location']['oneOf'][1]['type'] == 'object'
+    assert schema['components']['schemas']['X']['properties']['location']['oneOf'][1]['required'] == ['type', 'geometries']
+
+
+def test_serializer_field_extension_geojson(no_warnings):
+    class XSerializer(serializers.Serializer):
+        location = GeometrySerializerMethodField()
+
+    class XViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+        serializer_class = XSerializer
+
+    schema = generate_schema('x', XViewset)
+    validate_schema(schema)
+    assert isinstance(schema['components']['schemas']['X']['properties']['location'], dict)
+    assert len(schema['components']['schemas']['X']['properties']['location']['oneOf']) == 2
+    assert schema['components']['schemas']['X']['properties']['location']['oneOf'][0]['type'] == 'object'
+    assert schema['components']['schemas']['X']['properties']['location']['oneOf'][0]['required'] == ['id', 'type', 'geometry']
+    assert schema['components']['schemas']['X']['properties']['location']['oneOf'][1]['type'] == 'object'
+    assert schema['components']['schemas']['X']['properties']['location']['oneOf'][1]['required'] == ['id', 'type', 'features']
