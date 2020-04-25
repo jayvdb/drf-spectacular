9b48bbb0b42a60f82be71d4d869f2bc08025b2f6
John Vandenberg <jayvdb@gmail.com>
2020-04-25 10:49:25 +0700

    Add drf-gis fields

--- drf_spectacular/contrib/__init__.py
+++ drf_spectacular/contrib/__init__.py
@@ -2,6 +2,7 @@ __all__ = [
     'django_oauth_toolkit',
     'djangorestframework_camel_case',
     'rest_polymorphic',
+    'rest_framework_gis',
     'rest_framework_jwt',
     'rest_framework_simplejwt'
 ]
