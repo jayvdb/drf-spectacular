5533e536e7d6b4931fd57f26e8c8dc88b72d9e10
John Vandenberg <jayvdb@gmail.com>
2020-04-27 18:27:17 +0700

    Dont render errors as yaml

--- drf_spectacular/renderers.py
+++ drf_spectacular/renderers.py
@@ -1,11 +1,22 @@
 import yaml
 from rest_framework.renderers import OpenAPIRenderer
+#from drf_spectacular.plumbing import warn
 
+from rest_framework.exceptions import ErrorDetail
+
+
+import logging
+logger = logging.getLogger(__name__)
 
 class NoAliasOpenAPIRenderer(OpenAPIRenderer):
     """ Remove this temp fix once DRF 3.11 is no longer supported """
 
     def render(self, data, media_type=None, renderer_context=None):
+        logger.error(media_type)
+        if isinstance(data, ErrorDetail):
+            logger.error(data)
+            return False
+
         # disable yaml advanced feature 'alias' for clean, portable, and readable output
         class Dumper(yaml.SafeDumper):
             def ignore_aliases(self, data):
