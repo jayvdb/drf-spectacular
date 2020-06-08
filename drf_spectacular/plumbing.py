6148244206804b6f1231a647eef653ee6f63d2d9
John Vandenberg <jayvdb@gmail.com>
2020-06-08 14:07:51 +0700

    Revert "improve enum naming mechanism."
    This reverts commit 595d413ed130d02bcc441a84593928f1ede39eb3.

--- drf_spectacular/plumbing.py
+++ drf_spectacular/plumbing.py
@@ -1,4 +1,3 @@
-import hashlib
 import inspect
 import json
 import sys
@@ -522,6 +521,9 @@ def postprocess_schema_enums(result, generator, **kwargs):
     the same choices. Aids client generation to not generate a separate enum for
     every occurrence. only takes effect when replacement is guaranteed to be correct.
     """
+    def choice_hash(choices):
+        return hash(json.dumps(choices, sort_keys=True))
+
     def iter_prop_containers(schema):
         if isinstance(schema, list):
             for item in schema:
@@ -534,55 +536,34 @@ def postprocess_schema_enums(result, generator, **kwargs):
 
     schemas = result.get('components', {}).get('schemas', {})
 
-    overrides = load_enum_name_overrides()
+    overrides = {
+        choice_hash(list(dict(choices).keys())): name
+        for name, choices in spectacular_settings.ENUM_NAME_OVERRIDES.items()
+    }
 
     hash_mapping = defaultdict(set)
-    # collect all enums, their names and choice sets
+    # collect all enums, their names and contents
     for props in iter_prop_containers(list(schemas.values())):
         for prop_name, prop_schema in props.items():
             if 'enum' not in prop_schema:
                 continue
-            hash_mapping[prop_name].add(list_hash(prop_schema['enum']))
-
-    # traverse all enum properties and generate a name for the choice set. naming collisions
-    # are resolved and a warning is emitted. giving a choice set multiple names is technically
-    # correct but potentially unwanted. also emit a warning there to make the user aware.
-    enum_name_mapping = {}
-    for prop_name, prop_hash_set in hash_mapping.items():
-        for prop_hash in prop_hash_set:
-            if prop_hash in overrides:
-                enum_name = overrides[prop_hash]
-            elif len(prop_hash_set) == 1:
+            hash_mapping[prop_name].add(choice_hash(prop_schema['enum']))
+    # safe replacement requires name to have only one set of enum values
+    candidate_enums = {
+        prop_name for prop_name, prop_hash_set in hash_mapping.items()
+        if len(prop_hash_set) == 1
+    }
+    # replace all valid occurrences with enum schema component
+    for props in iter_prop_containers(list(schemas.values())):
+        for prop_name, prop_schema in props.items():
+            if 'enum' not in prop_schema:
+                continue
+            elif choice_hash(prop_schema['enum']) in overrides:
+                enum_name = overrides[choice_hash(prop_schema['enum'])]
+            elif prop_name in candidate_enums:
                 enum_name = f'{inflection.camelize(prop_name)}Enum'
             else:
-                enum_name = f'{inflection.camelize(prop_name)}{prop_hash[:3].capitalize()}Enum'
-                warn(
-                    f'automatic enum naming encountered a collision for field "{prop_name}". the '
-                    f'same name has been used for multiple choice sets. the collision was resolved '
-                    f'with {enum_name}. add an entry to ENUM_NAME_OVERRIDES to fix the naming.'
-                )
-            if enum_name_mapping.get(prop_hash, enum_name) != enum_name:
-                warn(
-                    f'encountered multiple names for the same choice set ({enum_name}). this '
-                    f'may be unwanted even though the generated schema is technically correct. '
-                    f'add an entry to ENUM_NAME_OVERRIDES to fix the naming.'
-                )
-                del enum_name_mapping[prop_hash]
-            else:
-                enum_name_mapping[prop_hash] = enum_name
-            enum_name_mapping[(prop_hash, prop_name)] = enum_name
-
-    # replace all enum occurrences with a enum schema component. cut out the
-    # enum, replace it with a reference and add a corresponding component.
-    for props in iter_prop_containers(list(schemas.values())):
-        for prop_name, prop_schema in props.items():
-            if 'enum' not in prop_schema:
                 continue
-
-            prop_hash = list_hash(prop_schema['enum'])
-            # when choice sets are reused under multiple names, the generated name cannot be
-            # resolved from the hash alone. fall back to prop_name and hash for resolution.
-            enum_name = enum_name_mapping.get(prop_hash) or enum_name_mapping[prop_hash, prop_name]
 
             enum_schema = {k: v for k, v in prop_schema.items() if k in ['type', 'enum']}
             prop_schema = {k: v for k, v in prop_schema.items() if k not in ['type', 'enum']}
--- tests/test_regressions.py
+++ tests/test_regressions.py
@@ -460,7 +460,6 @@ def test_custom_model_field_from_base_field(no_warnings):
 
 def test_follow_field_source_through_intermediate_property_or_function(no_warnings):
     class FieldSourceTraversalModel2(models.Model):
-        x = models.IntegerField(choices=[(1, '1'), (2, '2')])
         y = models.IntegerField(choices=[(1, '1'), (2, '2'), (3, '3')])
 
     class FieldSourceTraversalModel1(models.Model):
@@ -472,7 +471,7 @@ def test_follow_field_source_through_intermediate_property_or_function(no_warnin
             return  # pragma: no cover
 
     class XSerializer(serializers.ModelSerializer):
-        prop = serializers.ReadOnlyField(source='prop.x')
+        prop = serializers.ReadOnlyField(source='prop.y')
         func = serializers.ReadOnlyField(source='func.y')
 
         class Meta:
